cc0dc5495c1790b2afe137ef174b0e245c0557b6
Amrit Mishra <amritmishra2000@gmail.com>
2018-02-18 16:06:49 +0530

    initial commit

---
+++ my-router/router.js
@@ -0,0 +1,15 @@
+/**
+ * Created by amrmishr on 2/18/18.
+ */
+
+const Router = {
+    routes : [],
+    // will add Routes to the application which will use our router
+    // about us -->, contact,
+    addRoutes :function(){},
+
+    // will initalise the route
+    checkIn : function(){},
+    navigate : function(){}
+
+};
